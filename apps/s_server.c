0778cf623111b7c0b6574a6cbf0a2e27e9d4c052
Matt Caswell <matt@openssl.org>
2015-09-16 17:01:58 +0100

    Implement local thread pools
    Implement the ASYNC_JOB as a local thread pool. Remove the API support
    for global pools.

--- apps/s_server.c
+++ apps/s_server.c
@@ -1668,7 +1668,7 @@ int s_server_main(int argc, char *argv[])
 
     if (async) {
         SSL_CTX_set_mode(ctx, SSL_MODE_ASYNC);
-        ASYNC_init_pool(0, 0, 0);
+        ASYNC_init_pool(0, 0);
     }
 
 #ifndef OPENSSL_NO_SRTP
