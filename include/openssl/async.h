315795fc2879f64c37043613d04768408eafa381
root <root@sie-lab-213-208.ir.intel.com>
2015-11-27 21:40:39 +0000

    adding async in uio case

--- include/openssl/async.h
+++ include/openssl/async.h
@@ -75,6 +75,7 @@ int ASYNC_start_job(ASYNC_JOB **job, int *ret, int (*func)(void *),
 int ASYNC_pause_job(void);
 
 int ASYNC_get_wait_fd(ASYNC_JOB *job);
+void ASYNC_set_wait_fd(ASYNC_JOB *job, int fd);
 ASYNC_JOB *ASYNC_get_current_job(void);
 void ASYNC_wake(ASYNC_JOB *job);
 void ASYNC_clear_wake(ASYNC_JOB *job);
