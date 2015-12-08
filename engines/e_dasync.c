9add335f4dd23ff2ca852d819305714d560a8b2c
Abhi <abhishekX.karoliya@intel.com>
2015-12-08 18:18:07 +0000

    Change calls from RSA_PKCS1_SSLeay to RSA_PKCS1_OpenSSL

--- engines/e_dasync.c
+++ engines/e_dasync.c
@@ -327,7 +327,7 @@ static int dasync_rsa_priv_dec(int flen, const unsigned char *from,
 static int dasync_rsa_mod_exp(BIGNUM *r0, const BIGNUM *I, RSA *rsa, BN_CTX *ctx)
 {
     dummy_pause_job();
-    return RSA_PKCS1_SSLeay()->rsa_mod_exp(r0, I, rsa, ctx);
+    return RSA_PKCS1_OpenSSL()->rsa_mod_exp(r0, I, rsa, ctx);
 }
 
 static int dasync_rsa_init(RSA *rsa)
@@ -343,12 +343,12 @@ static int dasync_rsa_sign(int type, const unsigned char *m,
     unsigned int m_len, unsigned char *sigret, unsigned int *siglen, const RSA *rsa)
 {
     dummy_pause_job();
-    return RSA_PKCS1_SSLeay()->rsa_sign(type, m, m_len, sigret, siglen, rsa);
+    return RSA_PKCS1_OpenSSL()->rsa_sign(type, m, m_len, sigret, siglen, rsa);
 }
 
 static int dasync_rsa_verify(int dtype, const unsigned char *m,
     unsigned int m_len, const unsigned char *sigbuf, unsigned int siglen, const RSA *rsa)
 {
     dummy_pause_job();
-    return RSA_PKCS1_SSLeay()->rsa_verify(dtype, m, m_len, sigbuf, siglen, rsa);
+    return RSA_PKCS1_OpenSSL()->rsa_verify(dtype, m, m_len, sigbuf, siglen, rsa);
 }
